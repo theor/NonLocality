67e00470abac9afedead6114c9614b4eae588b99
Theo Richart <theo.richart@gmail.com>
2015-08-05 09:53:55 -0400

    init commit

---
+++ tests/NonLocality.Tests/Tests.fs
@@ -0,0 +1,10 @@
+module NonLocality.Tests
+
+open NonLocality
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  let result = Library.hello 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)
