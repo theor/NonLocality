4144b48755f26fee4688e5c6a8717a28dc05e75e
Theo Richart <theo.richart@gmail.com>
2015-08-06 14:41:10 -0400

    mahapps integration

--- src/NonLocality/Library.fs
+++ src/NonLocality/Library.fs
@@ -1,13 +1,15 @@
 module NonLocality
 
-open System.Windows
-open FSharp.Qualia
 open System.Collections.ObjectModel
+open System.Windows
 open System.Windows.Controls
-open FSharp.Qualia.WPF
 open System
-open NonLocality.Lib
+
 open Amazon.S3
+open FsXaml
+open FSharp.Qualia
+open FSharp.Qualia.WPF
+open NonLocality.Lib
 
 let cast<'a> (x:obj) : 'a option =
     match x with
@@ -16,20 +18,7 @@ let cast<'a> (x:obj) : 'a option =
 
 type Events = Fetch | Remove | SelectionChanged of ControlledFile.T option
 
-type ListViewWindow() as x =
-    inherit Window()
-    let label = Label()
-    let button = Button(Content="Add Item")
-    let list = ListBox()
-    do
-        let sp = StackPanel()
-        sp.Children.Add button |> ignore
-        sp.Children.Add label |> ignore
-        sp.Children.Add list |> ignore
-        x.Content <- sp
-    member val Label = label
-    member val Button = button
-    member val List = list
+type ListViewWindow = XAML<"SyncWindow.xaml", true>
 
 type ItemView(m) =
     inherit View<Events, Label, ControlledFile.T>(Label(), m)
@@ -40,21 +29,21 @@ type ListModel() =
     member val Items = new ObservableCollection<ControlledFile.T>()
     member val SelectedItem = new ReactiveProperty<ControlledFile.T option>(None)
 
-type ListAView(elt, m) =
-    inherit DerivedCollectionSourceView<Events, ListViewWindow, ListModel>(elt, m)
+type ListAView(elt:ListViewWindow, m) =
+    inherit DerivedCollectionSourceView<Events, Window, ListModel>(elt.Root, m)
 
     override x.EventStreams = [
         (** Add an item when clicking the Add button *)
-        elt.Button.Click --> Fetch
+        elt.button.Click --> Fetch
         (** This one just fetch the selected item, tries to cast it to ItemView, then select the view's Model as an option. *)
-        elt.List.SelectionChanged |> Observable.map (fun _ -> SelectionChanged((cast<ItemView> elt.List.SelectedItem |> Option.map(fun v -> v.Model))))
+        elt.list.SelectionChanged |> Observable.map (fun _ -> SelectionChanged((cast<ItemView> elt.list.SelectedItem |> Option.map(fun v -> v.Model))))
         (** Send a remove event, only when <Del> is pressed *)
-        elt.List.KeyDown |> Observable.filter (fun (e:Input.KeyEventArgs) -> e.Key = Input.Key.Delete) |> Observable.mapTo Remove ]
+        elt.list.KeyDown |> Observable.filter (fun (e:Input.KeyEventArgs) -> e.Key = Input.Key.Delete) |> Observable.mapTo Remove ]
     override x.SetBindings m =
         (** That's all the collection plumbing: which WPF list, how to create a view for each item model, and which model collection to monitor.
             We could use the returned CollectionView to do some filtering/grouping/sorting/... *)
-        let collview = x.linkCollection elt.List (fun i -> ItemView(i)) m.Items
-        m.SelectedItem |> Observable.add (fun i -> elt.Label.Content <- sprintf "Press <DEL> to delete the selection item. Current Selection: %A" i)
+        let collview = x.linkCollection elt.list (fun i -> ItemView(i)) m.Items
+        m.SelectedItem |> Observable.add (fun i -> elt.label.Content <- sprintf "Press <DEL> to delete the selection item. Current Selection: %A" i)
         ()
 (**
 Typical dispatcher - 
@@ -78,6 +67,8 @@ type ListController(s3:IAmazonS3, sp:SyncPoint.T) =
             | Remove -> Sync (fun m -> m.SelectedItem.Value |> Option.iter (m.Items.Remove >> ignore))
             | SelectionChanged item -> printfn "%A" item; Sync (fun m -> m.SelectedItem.Value <- item)
 
+type App = XAML<"App.xaml">
+
 [<EntryPoint>]
 [<STAThread>]
 let main args =
@@ -88,11 +79,11 @@ let main args =
         let s3 = Profiles.createClient pp
 //        let buckets = SyncPoint.listBuckets s3
         let sp = SyncPoint.create "sync-bucket-test" "F:\\tmp\\nonlocality"  [||] SyncTrigger.Manual
-        let app = Application()
+        let app = App()
         let lm = ListModel()
         let v = ListAView(new ListViewWindow(),lm)
         let c = ListController(s3, sp)
         let loop = EventLoop(v, c)
         use l = loop.Start()
-        app.Run(v.Root)
+        app.Root.Run(v.Root)
 
