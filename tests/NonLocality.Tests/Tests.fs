3b179f01b250edb509be122d26568135a6e8b321
Theo Richart <theo.richart@gmail.com>
2015-08-05 21:51:16 -0400

    init

--- tests/NonLocality.Tests/Tests.fs
+++ tests/NonLocality.Tests/Tests.fs
@@ -1,10 +1,9 @@
 module NonLocality.Tests
 
-open NonLocality
 open NUnit.Framework
 
 [<Test>]
 let ``hello returns 42`` () =
-  let result = Library.hello 42
+  let result = 42
   printfn "%i" result
   Assert.AreEqual(42,result)
