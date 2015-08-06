1927b05b7d3494c21d296f653ddfbefd339b5c7f
Theo Richart <theo.richart@gmail.com>
2015-08-05 21:54:17 -0400

    lib

---
+++ src/NonLocality.Lib/Library1.fs
@@ -0,0 +1,17 @@
+﻿namespace NonLocality.Lib
+
+open Amazon.Util
+
+module Profiles =
+    open Amazon
+
+    let getProfile =
+        let l = ProfileManager.ListProfileNames() |> List.ofSeq    
+        match l with
+        | [] -> None
+        | p :: _ -> Some (ProfileManager.GetAWSCredentials(p))
+    let createClient (p:Amazon.Runtime.AWSCredentials) =
+        AWSClientFactory.CreateAmazonS3Client(p, Amazon.RegionEndpoint.USEast1)
+    
+type SyncPoint(bucketName) =
+    member x.BucketName = bucketName
